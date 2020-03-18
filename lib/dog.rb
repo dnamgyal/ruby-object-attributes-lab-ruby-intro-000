d4dbf1ddf9591089b6615a6c29fa65fe84ab1afa
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-18 03:51:25 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(name=, breed)
+    @name = name=
+    @breed = breed
+  end
+
+  def name(name)
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed(breed)
+    @breed = breed
+  end
+
+  def breed=(new_breed)
+    @breed = breed
+  end
+
+end
